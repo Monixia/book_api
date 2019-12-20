0ec25170f56d1d86818dab7937ec4d77d8366d89
Gulnara Sirazetdinova <g.sirazetdinova@dsec.ru>
2019-12-20 19:37:16 +0300

    update configs

--- api/server/src/config/config.js
+++ api/server/src/config/config.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    database: 'collector',
-    username: 'postgres',
-    password: 'postgres',
-    host: '172.16.2.51',
+    database: 'book',
+    username: 'root',
+    password: null,
+    host: '127.0.0.1',
     port: '5432',
     dialect: 'postgres'
   },
